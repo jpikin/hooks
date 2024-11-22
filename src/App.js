6b5df8e445a03532be0dc5bc9491092de442c317
Pikin <pikin@bk.ru>
2024-11-22 17:28:30 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import classes from "./App.module.css";
+import Buttons from "./components/Buttons";
 
-function App() {
+const App = () => {
+  const [color, setColor] = useState('aqua');
+  const [isPopupOpen, setIsPopupOpen] = useState(false); 
+  const popupRef = useRef(null); 
+
+  function chColor() {
+    color === 'aqua' ? setColor('aquamarine') : setColor('aqua');
+  }
+
+  function togglePopup() {
+    setIsPopupOpen(true);
+  }
+
+  
+  function handleClickOutside(event) {
+    if (popupRef.current && !popupRef.current.contains(event.target)) {
+      setIsPopupOpen(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('click', chColor);
+    document.body.style.backgroundColor = color;
+    document.querySelector(`.${classes.link}`).addEventListener('click', togglePopup);
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      window.removeEventListener('click', chColor);
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [color, isPopupOpen]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      <h2 className={classes.title}>CSS Module Buttons</h2>
+      <div className={classes.wrapper}>
+        <Buttons />
+      </div>
+      <a href="#" className={classes.link}>Открыть окошко</a>
+      <div
+        ref={popupRef}
+        className={`${classes.popup} ${isPopupOpen ? classes.show : ''}`}
+      ></div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Buttons.js
@@ -0,0 +1,11 @@
+import styles from "./Buttons.module.css"; 
+
+const Buttons = () => (
+	<>
+		<button className={styles.btn1}>btn1</button>
+		<button className={styles.btn3}>btn3</button>
+		<button className={styles.btn2}>btn2</button>
+	</>
+);
+
+export default Buttons;
